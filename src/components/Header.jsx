646c7c6211e1184cd563c725438d41caa7b2a43c
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-04-02 14:00:05 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,15 +12,15 @@ const Header=()=>{
         
         <div className=" top-0 w-full h-full flex flex-col justify-center text-center text-white">
             <h2 className="text-3xl md:text-4xl p-4">First class Travel</h2>
-            <h3 className="text-2xl md:text-3xl pb-2">Top 1% locations <ReactTyped className="text-white
-            " strings={['Worldwide']} 
-                    typeSpeed={340}
-                    backSpeed={200}
+            <h3 className="text-2xl md:text-3xl pb-2"> <ReactTyped className="text-white
+            " strings={['Top 1% locations Worldwide']} 
+                    typeSpeed={40}
+                    backSpeed={20}
                     loop/>  </h3>
            
             <form className="flex justify-between items-center mx-auto max-w-[600px] w-full border-2 border-white p-1 text-black rounded-2xl bg-gray-100">
                 <div >
-                    <input className=" sm:w-[500px] focus:outline-none font-semibold placeholder:text-pink-600" type="text" placeholder="Search Destination" />
+                    <input className=" sm:w-[500px] focus:outline-none font-semibold placeholder:text-cyan-600" type="text" placeholder="Search Destination" />
                 </div>
                 <div className="px-5 ">
                     <button><AiOutlineSearch size={20}/>
